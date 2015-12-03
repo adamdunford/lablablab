408be63ac46be87be0cffb2683c221d60807d155
Adam Dunford <adamdunford@gmail.com>
2015-12-03 14:29:30 +0100

    first commit of project

---
+++ Lablablab 1.0/Lablablab 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lablablab 1.0
+//
+//  Created by Adam Dunford on 12/3/15.
+//  Copyright © 2015 CIU196 Mobile Computing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
